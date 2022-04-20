39eafcffe2f980835e2ddc2282ce6c4b028483bb
Morgen Huether <huetherml@jacks.sfasu.edu>
2022-04-20 13:32:01 -0500

    Minor visual changes to launch page

--- app/src/main/java/com/example/moneybot_virtual_wallet/LoginLaunch.java
+++ app/src/main/java/com/example/moneybot_virtual_wallet/LoginLaunch.java
@@ -10,18 +10,38 @@ import android.widget.Button;
 public class LoginLaunch extends AppCompatActivity implements View.OnClickListener {
 
     @Override
-    public void onClick(View view) {
-        Intent login = new Intent(this, MainHome.class);
-        startActivity(login);
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_launch);
 
+        // create button click listeners
         Button btnLogin = findViewById(R.id.btn_Login);
         btnLogin.setOnClickListener(this);
+        Button btnCreate = findViewById(R.id.btn_CreateAcc);
+        btnCreate.setOnClickListener(this);
     }
+
+    // When clicked...
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            // If the login button was clicked, launch login page
+            case R.id.btn_Login:
+                Intent login = new Intent(this, MainHome.class);
+                startActivity(login);
+                break;
+
+            // If the create account button was clicked, launch create page
+            case R.id.btn_CreateAcc:
+                Intent create = new Intent(this, LoginCreate.class);
+                startActivity(create);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
 
 }
