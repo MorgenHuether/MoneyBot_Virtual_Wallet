62dfcc392f2e86cde628c84f4c26a19c47fdd576
Morgen Huether <huetherml@jacks.sfasu.edu>
2022-04-20 11:29:18 -0500

    Renamed MainActivity to MainHome, refactored in all main pages

--- app/src/main/java/com/example/moneybot_virtual_wallet/MainHome.java
+++ app/src/main/java/com/example/moneybot_virtual_wallet/MainHome.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_launch);
+        setContentView(R.layout.main_home);
     }
 
 
