77591caca278e94811d93ff454f2e3997b581bec
Morgen Huether <huetherml@jacks.sfasu.edu>
2022-04-24 15:46:04 -0500

    Added NFC ppopup window. Still needs to be fully implemented

--- app/src/main/java/com/example/moneybot_virtual_wallet/MainPay.java
+++ app/src/main/java/com/example/moneybot_virtual_wallet/MainPay.java
@@ -1,10 +1,13 @@
 package com.example.moneybot_virtual_wallet;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPay extends AppCompatActivity implements View.OnClickListener {
@@ -25,6 +28,8 @@ public class MainPay extends AppCompatActivity implements View.OnClickListener {
         btnFriends.setOnClickListener(this);
         Button btnSettings = findViewById(R.id.navbtn_Settings);
         btnSettings.setOnClickListener(this);
+        Button btnNFCStart = findViewById(R.id.btn_StartNFC);
+        btnNFCStart.setOnClickListener(this);
 
     }
 
@@ -60,6 +65,13 @@ public class MainPay extends AppCompatActivity implements View.OnClickListener {
             case R.id.navbtn_Settings:
                 Intent set = new Intent(this, MainSettings.class);
                 startActivity(set);
+                break;
+
+            // If NFC pay button was clicked, show NFC popup
+            case R.id.btn_StartNFC:
+
+                // TODO: display popup for NFC "nfc_paypopup.xml"
+
                 break;
             default:
                 break;
