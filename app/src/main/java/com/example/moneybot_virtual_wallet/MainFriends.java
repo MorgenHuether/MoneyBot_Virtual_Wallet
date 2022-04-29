84f6979c3574b414c2dc55ff319bab1d2e52770e
Morgen Huether <huetherml@jacks.sfasu.edu>
2022-04-29 15:09:15 -0500

    Cleaned up Friends page. TODO: display friends and implement search function

--- app/src/main/java/com/example/moneybot_virtual_wallet/MainFriends.java
+++ app/src/main/java/com/example/moneybot_virtual_wallet/MainFriends.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainFriends extends AppCompatActivity implements View.OnClickListener {
+public class MainFriends extends AppCompatActivity {
 
     Bundle extras;
 
@@ -19,10 +19,12 @@ public class MainFriends extends AppCompatActivity implements View.OnClickListen
         // Contains username info passed from last activity
         extras = getIntent().getExtras();
 
-        // create button click listeners
-        Button btnHome = findViewById(R.id.navbtn_Home);
-//        btnHome.setOnClickListener(this);
+        // TODO: Display current friends
+        // TODO: Implement friends search
 
+
+        // If Home clicked
+        Button btnHome = findViewById(R.id.navbtn_Home);
         btnHome.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), MainHome.class);
             if (extras != null)
@@ -30,9 +32,8 @@ public class MainFriends extends AppCompatActivity implements View.OnClickListen
             startActivity(intent);
         });
 
+        // If nav Move Money clicked
         Button btnMove = findViewById(R.id.navbtn_MoveMoney);
-//        btnMove.setOnClickListener(this);
-
         btnMove.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), MainMoveFunds.class);
             if (extras != null)
@@ -40,9 +41,8 @@ public class MainFriends extends AppCompatActivity implements View.OnClickListen
             startActivity(intent);
         });
 
+        // If nav Add Money clicked
         Button btnAdd = findViewById(R.id.navbtn_AddMoney);
-//        btnAdd.setOnClickListener(this);
-
         btnAdd.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), MainAddFunds.class);
             if (extras != null)
@@ -50,9 +50,8 @@ public class MainFriends extends AppCompatActivity implements View.OnClickListen
             startActivity(intent);
         });
 
+        // If nav Pay clicked
         Button btnPay = findViewById(R.id.navbtn_Pay);
-//        btnPay.setOnClickListener(this);
-
         btnPay.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), MainPay.class);
             if (extras != null)
@@ -60,9 +59,8 @@ public class MainFriends extends AppCompatActivity implements View.OnClickListen
             startActivity(intent);
         });
 
+        // If nav Settings clicked
         Button btnSettings = findViewById(R.id.navbtn_Settings);
-//        btnSettings.setOnClickListener(this);
-
         btnSettings.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), MainSettings.class);
             if(extras != null)
@@ -72,42 +70,5 @@ public class MainFriends extends AppCompatActivity implements View.OnClickListen
 
     }
 
-    // When clicked...
-    public void onClick(View view) {
-        switch (view.getId()){
-
-            // If Home nav button was clicked, launch page
-            case R.id.navbtn_Home:
-                Intent home= new Intent(this, MainHome.class);
-                startActivity(home);
-                break;
-
-            // If MoveMoney nav button was clicked, launch page
-            case R.id.navbtn_MoveMoney:
-                Intent move = new Intent(this, MainMoveFunds.class);
-                startActivity(move);
-                break;
-
-            // If AddMoney nav button was clicked, launch page
-            case R.id.navbtn_AddMoney:
-                Intent add = new Intent(this, MainAddFunds.class);
-                startActivity(add);
-                break;
-
-            // If NFC Pay nav button was clicked, launch page
-            case R.id.navbtn_Pay:
-                Intent pay = new Intent(this, MainPay.class);
-                startActivity(pay);
-                break;
-
-            // If Settings nav button was clicked, launch page
-            case R.id.navbtn_Settings:
-                Intent set = new Intent(this, MainSettings.class);
-                startActivity(set);
-                break;
-            default:
-                break;
-        }
-    }
 
 }
