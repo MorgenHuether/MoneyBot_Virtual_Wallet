66353b7c46fdc5bcf18a2a9be1257946b80e8cd4
Ian <stephensia@jacks.sfasu.edu>
2022-04-29 18:10:29 -0500

    Updated DBHelper and BankAdd. Added Card class. Added more sql tables to DBHelper and methods of accessing table info

--- app/src/main/java/com/example/moneybot_virtual_wallet/BankAdd.java
+++ app/src/main/java/com/example/moneybot_virtual_wallet/BankAdd.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,10 +13,19 @@ public class BankAdd extends AppCompatActivity implements View.OnClickListener{
 
     Bundle extras;
 
+    EditText bankname, cardholder, account, routing;
+    DBHelper DB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bank_add);
+
+        bankname = findViewById(R.id.txtbx_BankName);
+        cardholder = findViewById(R.id.txtbx_CardName);
+        account = findViewById(R.id.txtbx_AccountNum);
+        routing = findViewById(R.id.txtbx_RoutingNum);
+        DB = new DBHelper(this);
 
         // create button click listeners
         Button btnAdd = findViewById(R.id.btn_AddBank);
@@ -22,13 +33,28 @@ public class BankAdd extends AppCompatActivity implements View.OnClickListener{
 
         // If add bank clicked, add to DB and return to home
         btnAdd.setOnClickListener(view -> {
-            //TODO: add bank information input to DB
+            String bank = bankname.getText().toString();
+            String card = cardholder.getText().toString();
+            String acc = account.getText().toString();
+            String rout = routing.getText().toString();
 
-
-            Intent intent = new Intent(getApplicationContext(), MainSettings.class);
-            if(extras != null)
-                intent.putExtras(extras);
-            startActivity(intent);
+            // If left blank
+            if(bank.equals("")||card.equals("")||acc.equals("")||rout.equals(""))
+                Toast.makeText(BankAdd.this, "Missing required information", Toast.LENGTH_SHORT).show();
+            else {
+                // If db does not already contain account, store new account data
+                if(!DB.checkBankAccount(acc))
+                    if(DB.insertBankData(bank, card, acc, rout, extras.getString("username"))) {
+                        Toast.makeText(BankAdd.this, "Bank added successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), MainSettings.class);
+                        if(extras != null)
+                            intent.putExtras(extras);
+                        startActivity(intent);
+                    } else
+                        Toast.makeText(BankAdd.this, "Failed to add bank", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(BankAdd.this, "Account already exists", Toast.LENGTH_SHORT).show();
+            }
         });
 
         Button btnCancel = findViewById(R.id.btn_Cancel);
--- app/src/main/java/com/example/moneybot_virtual_wallet/DBHelper.java
+++ app/src/main/java/com/example/moneybot_virtual_wallet/DBHelper.java
@@ -12,19 +12,27 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String DBNAME = "Login.db";
 
     public DBHelper(Context context) {
-        super(context, "Login.db", null, 1);
+        super(context, DBNAME, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqlDB) {
         sqlDB.execSQL("create Table users(username TEXT primary key, password TEXT, email TEXT unique, phone TEXT)");
+        sqlDB.execSQL("create Table cards(cardnum TEXT primary key, expiration TEXT, cvv TEXT, username TEXT)");
+        sqlDB.execSQL("create Table banks(account TEXT primary key, routing TEXT, bankname TEXT, cardholder TEXT)");
+        sqlDB.execSQL("create Table user_bank_accounts(account TEXT primary key ,username TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqlDB, int i, int i1) {
         sqlDB.execSQL("drop Table if exists users");
+        sqlDB.execSQL("drop Table if exists cards");
+        sqlDB.execSQL("drop Table if exists banks");
+        sqlDB.execSQL("drop Table if exists user_bank_accounts");
+        onCreate(sqlDB);
     }
 
+    // USER LOGIN DATA METHODS
     public boolean insertData(String username, String password, String email, String phone) {
         SQLiteDatabase sqlDB = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
@@ -34,7 +42,7 @@ public class DBHelper extends SQLiteOpenHelper {
         contentValues.put("phone", phone);
         long result = sqlDB.insert("users", null, contentValues);
 
-        // If insertion fails
+        // If insertion succeeds
         return result != -1;
     }
 
@@ -60,4 +68,67 @@ public class DBHelper extends SQLiteOpenHelper {
 
         return cursor.getCount() > 0;
     }
+
+    protected String[] getUserInfo(String username) {
+        SQLiteDatabase sqlDB = this.getWritableDatabase();
+        Cursor cursor = sqlDB.rawQuery("Select * from users where username = ?", new String[] {username});
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        return new String[] {cursor.getString(0),cursor.getString(1),cursor.getString(2),cursor.getString(3)};
+    }
+
+    // CARDS TABLE METHODS
+    public boolean insertCardData(String card, String expiration, String cvv, String username) {
+        SQLiteDatabase sqlDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("card", card);
+        contentValues.put("expiration", expiration);
+        contentValues.put("cvv", cvv);
+        contentValues.put("username", username);
+        long result = sqlDB.insert("cards", null, contentValues);
+
+        // If insertion succeeds    -- If it does not then generate new random card number
+        return result != -1;
+    }
+
+    protected Card getCard(String card) {
+        SQLiteDatabase sqlDB = this.getWritableDatabase();
+        Cursor cursor = sqlDB.rawQuery("Select * from cards where card = ?", new String[] {card});
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        return new Card(cursor.getString(0),cursor.getString(1),cursor.getString(2),cursor.getString(3));
+    }
+
+    // BANKS TABLE METHODS
+    public boolean insertBankData(String bankname, String cardholder, String account, String routing, String username) {
+        SQLiteDatabase sqlDB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("account", account);
+        contentValues.put("routing", routing);
+        contentValues.put("bankname", bankname);
+        contentValues.put("cardholder", cardholder);
+        long result = sqlDB.insert("banks", null, contentValues);
+
+        if(result != -1) {
+            ContentValues cv = new ContentValues();
+            cv.put("account", account);
+            cv.put("username", username);
+            sqlDB.insert("user_bank_accounts", null, cv);
+        }
+
+        // If insertion succeeds
+        return result != -1;
+    }
+
+    public boolean checkBankAccount(String account) {
+        SQLiteDatabase sqlDB = this.getWritableDatabase();
+        Cursor cursor = sqlDB.rawQuery("Select * from banks where account = ?", new String[] {account});
+
+        // If account already exists in database
+        return cursor.getCount() > 0;
+    }
+
+
 }
