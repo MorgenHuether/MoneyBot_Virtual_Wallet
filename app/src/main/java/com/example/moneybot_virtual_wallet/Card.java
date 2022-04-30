df0660d66eb4b6a1c3394b5c9c4920f84aeda59c
Morgen Huether <huetherml@jacks.sfasu.edu>
2022-04-29 21:23:59 -0500

    fixed getUser bug

--- app/src/main/java/com/example/moneybot_virtual_wallet/Card.java
+++ app/src/main/java/com/example/moneybot_virtual_wallet/Card.java
@@ -5,16 +5,16 @@ public class Card {
     private String cardNumber, expiration, cvv, username;
 
     public Card(String username, String cardNumber, String expiration, String cvv) {
+        this.username = username;
         this.cardNumber = cardNumber;
         this.expiration = expiration;
         this.cvv = cvv;
-        this.username = username;
     }
 
     public String getCardNumber() { return this.cardNumber; }
     public String getExpiration() { return this.expiration; }
     public String getCvv() { return this.cvv; }
-    public String getUsername() { return this.cvv; }
+    public String getUsername() { return this.username; }
 
     public String[] getCardInfo() {
         return new String[]{this.username, this.cardNumber,this.cvv, this.expiration};
